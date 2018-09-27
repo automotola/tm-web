1d65ce32863263e0636df300a95b329e951b5e65
Omotola Bolarin <bolarin@omoto.la>
2018-09-27 10:51:46 +0200

    Save Home Page

---
+++ components/organisms/index.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .container
+    template(v-if="options.graph === true") 
+      graph
+      table
+    template(v-if="options.scene === true") 
+      field
+      table
+    template(v-if="options.map === true") 
+      map
+      table
+    template(v-if="options.netowrk === true") 
+      network
+      table
+</template>
+<script>
+import scene from '~/components/organisms/scene'
+import graph from '~/components/organisms/graph'
+import map from '~/components/organisms/scene'
+import network from '~/components/organisms/scene'
+
+import table from '~/components/molecules/table'
+
+export default {
+  components: {
+    graph,
+    scene,
+    map,
+    network
+  },
+  props: [
+    'options'
+  ]
+}
+</script>
