15278b980aed419202d43e22b2e06c43fcf88b3e
Omotola Bolarin <bolarin@omoto.la>
2018-10-25 17:03:01 +0200

    Further Minified

--- layouts/abrie.vue
+++ layouts/abrie.vue
@@ -2,3 +2,14 @@
   .uk-background-muted(uk-height-viewport).uk-flex.uk-flex-center.uk-flex-top
     nuxt
 </template>
+
+<style>
+.rotated {
+  animation: rotate 2s infinite;
+}
+@keyframes rotate {
+    100% {
+        transform: rotateY(180deg);
+    }
+}
+</style>
--- layouts/marcus.vue
+++
@@ -1,4 +0,0 @@
-<template lang="pug">
-  .uk-background-secondary(uk-height-viewport).uk-flex.uk-flex-center.uk-flex-middle
-    nuxt
-</template>
